cb9aa0ffd830f8dfe86d44c4284bbdcd9134b20e
Ali Aldemir <ali.aldemir@biletkolik.com>
2019-06-02 22:07:07 +0300

    added to IntegrationEventLogEF

---
+++ src/BuildingBlocks/EventBus/ContestPark.EventBus.IntegrationEventLogEF/Services/IIntegrationEventLogService.cs
@@ -0,0 +1,21 @@
+﻿using ContestPark.EventBus.Events;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ContestPark.EventBus.IntegrationEventLogEF.Services
+{
+    public interface IIntegrationEventLogService
+    {
+        Task<IEnumerable<IntegrationEventLogEntry>> RetrieveEventLogsPendingToPublishAsync(Guid transactionId);
+
+        Task SaveEventAsync(IntegrationEvent @event, IDbContextTransaction transaction);
+
+        Task MarkEventAsPublishedAsync(Guid eventId);
+
+        Task MarkEventAsInProgressAsync(Guid eventId);
+
+        Task MarkEventAsFailedAsync(Guid eventId);
+    }
+}
