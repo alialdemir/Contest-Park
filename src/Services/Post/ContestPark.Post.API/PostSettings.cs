8d58cb79beeafea694b46df47f74c098b694dd9c
Ali Aldemir <ali.aldemir@biletkolik.com>
2019-06-29 13:55:18 +0300

    kullanıcı event handlers yazıldı

--- src/Services/Post/ContestPark.Post.API/PostSettings.cs
+++ src/Services/Post/ContestPark.Post.API/PostSettings.cs
@@ -2,5 +2,8 @@
 {
     public class PostSettings
     {
+        public string Audience { get; set; }
+        public string ClientKey { get; set; }
+        public string identityUrl { get; set; }
     }
 }
