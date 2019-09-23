b244ec5c8fcda6aae852e82bb5fba65deb6a03ff
Ali Aldemir <ali.aldemir@biletkolik.com>
2019-09-23 23:20:31 +0300

    SP DÜZELTME

--- src/Services/Duel/ContestPark.Duel.API/Migrations/Scripts/SP_RandomQuestions.sql
+++ src/Services/Duel/ContestPark.Duel.API/Migrations/Scripts/SP_RandomQuestions.sql
@@ -6,16 +6,23 @@ IN founderLanguge TINYINT,
 IN opponentLanguge TINYINT
 ) 
 BEGIN 
-SELECT q.QuestionId, q.Link, q.AnswerType, q.QuestionType, ql.Question, ql.Language, al.CorrectStylish, al.Stylish1, al.Stylish2, al.Stylish3
-FROM Questions q
-INNER JOIN QuestionOfQuestionLocalizeds qoql ON qoql.QuestionId = q.QuestionId
-INNER JOIN QuestionLocalizeds ql ON ql.QuestionLocalizedId = qoql.QuestionLocalizedId AND ql.Language IN (founderLanguge, opponentLanguge)
-INNER JOIN AnswerLocalizeds al ON al.QuestionId = q.QuestionId AND al.Language = ql.Language
+
+SELECT 
+ddd.QuestionId, ddd.Link, ddd.AnswerType, ddd.QuestionType, ql.Question, ql.Language, al.CorrectStylish, al.Stylish1, al.Stylish2, al.Stylish3
+
+ FROM QuestionOfQuestionLocalizeds qoql, (
+SELECT q.QuestionId, q.Link, q.AnswerType, q.QuestionType  FROM Questions q
 WHERE q.IsActive = true
 AND q.SubCategoryId = subCategoryId
 AND q.QuestionId NOT IN (SELECT aq.QuestionId
 FROM AskedQuestions aq
 WHERE aq.QuestionId = q.QuestionId AND aq.UserId IN (founderUserId, opponentUserId))
 ORDER BY RAND()
-LIMIT 14;
+LIMIT 7
+) AS ddd, QuestionLocalizeds ql, AnswerLocalizeds al
+      WHERE qoql.QuestionId = ddd.QuestionId  AND
+		  ql.QuestionLocalizedId = qoql.QuestionLocalizedId AND
+		   ql.`Language` IN (founderLanguge, opponentLanguge) AND 
+		   al.QuestionId = qoql.QuestionId AND
+			 al.`Language` = ql.`Language`;
 END;
