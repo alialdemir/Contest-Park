3343079358796edb6e6118c6f9b759ccc9fc966b
Ali Aldemir <ali.aldemir@biletkolik.com>
2019-10-12 14:02:51 +0300

    bakiye kodu gir tabbar gizlendi

--- src/Mobile Apps/ContestPark.Mobile/ContestPark.Mobile/Views/BalanceCodeView.xaml.cs
+++ src/Mobile Apps/ContestPark.Mobile/ContestPark.Mobile/Views/BalanceCodeView.xaml.cs
@@ -1,12 +1,19 @@
 ﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace ContestPark.Mobile.Views
 {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class BalanceCodeView : ContentPage
     {
+        #region Constructor
+
         public BalanceCodeView()
         {
             InitializeComponent();
+            Shell.SetTabBarIsVisible(this, false);// Altta tabbar gözükmemesi için ekledim
         }
+
+        #endregion Constructor
     }
 }
