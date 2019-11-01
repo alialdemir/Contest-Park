018d9f2ffb14e8e9a31156f5e386f210fbd3c513
Ali Aldemir <ali.aldemir@biletkolik.com>
2019-11-01 16:12:19 +0300

    admin startup.cs

--- src/Services/Admin/ContestPark.Admin.API/Startup.cs
+++ src/Services/Admin/ContestPark.Admin.API/Startup.cs
@@ -1,8 +1,15 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using ContestPark.Admin.API.Resources;
+using ContestPark.Core.Middlewares;
+using ContestPark.EventBus.Abstractions;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
 
 namespace ContestPark.Admin.API
 {
@@ -16,20 +23,66 @@ namespace ContestPark.Admin.API
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.Configure<AdminSettings>(Configuration);
+
+            services.AddAuth(Configuration)
+                    .AddMySql()
+                    .AddMvc()
+                    .AddJsonOptions()
+                    .AddDataAnnotationsLocalization(typeof(AdminResource).Name)
+                    .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddLocalizationCustom();
+
+            services
+                    .AddRabbitMq(Configuration)
+                    .AddCorsConfigure();
+
+            var container = new ContainerBuilder();
+            container.Populate(services);
+            return new AutofacServiceProvider(container.Build());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvc();
+            var pathBase = Configuration["PATH_BASE"];
+            if (!string.IsNullOrEmpty(pathBase))
+            {
+                loggerFactory.CreateLogger("init").LogDebug($"Using PATH BASE '{pathBase}'");
+                app.UsePathBase(pathBase);
+            }
+
+            app.UseExceptionHandlerConfigure()
+               .AddCors();
+
+            ConfigureAuth(app);
+
+            app.UseRequestLocalizationCustom()
+               .UseMvc();
+
+            ConfigureEventBus(app);
+        }
+
+        protected virtual void ConfigureAuth(IApplicationBuilder app)
+        {
+            app
+                .UseMiddleware<ServiceAuthorizeMiddleware>()
+                .UseAuth();
+        }
+
+        protected virtual void ConfigureEventBus(IApplicationBuilder app)
+        {
+            var eventBus = app.ApplicationServices.GetRequiredService<IEventBus>();
+
+            // eventBus.Subscribe<ChangeBalanceIntegrationEvent, ChangeBalanceIntegrationEventHandler>();
         }
     }
 }
