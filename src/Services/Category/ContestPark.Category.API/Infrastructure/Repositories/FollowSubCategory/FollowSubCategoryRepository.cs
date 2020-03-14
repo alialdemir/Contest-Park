38a84e88b69ef001a1ab70c4f7de54482681895b
Ali Aldemir <ali.aldemir@biletkolik.com>
2020-03-14 09:25:36 +0300

    takip edilen kategoriler search

--- src/Services/Category/ContestPark.Category.API/Infrastructure/Repositories/FollowSubCategory/FollowSubCategoryRepository.cs
+++ src/Services/Category/ContestPark.Category.API/Infrastructure/Repositories/FollowSubCategory/FollowSubCategoryRepository.cs
@@ -2,7 +2,6 @@
 using ContestPark.Core.Database.Interfaces;
 using ContestPark.Core.Database.Models;
 using ContestPark.Core.Enums;
-using ContestPark.Core.Models;
 using System.Threading.Tasks;
 
 namespace ContestPark.Category.API.Infrastructure.Repositories.FollowSubCategory
@@ -90,58 +89,24 @@ namespace ContestPark.Category.API.Infrastructure.Repositories.FollowSubCategory
         /// <returns>Alt kategori id</returns>
         public ServiceModel<SearchModel> FollowedSubCategoryIds(string searchText, string userId, Languages language, PagingModel pagingModel)
         {
-            string sql = $@"SELECT
-                                      scl.SubCategoryName,
-                                      sc.SubCategoryId,
-                                      2 AS SearchType,
-                                      cl.Text AS CategoryName,
-                                      (case (SELECT
-                                      (CASE
-                                      WHEN EXISTS(
-                                      SELECT NULL
-                                      FROM OpenSubCategories AS osc  where osc.UserId = @userId and osc.SubCategoryId = sc.SubCategoryId)
-                                      THEN 1
-                                      ELSE 0
-                                      END) )
-                                      when 1 then 0
-                                      else sc.DisplayPrice
-                                      end) as DisplayPrice,
-
-                                      (case (SELECT
-                                      (CASE
-                                      WHEN EXISTS(
-                                      SELECT NULL
-                                      FROM OpenSubCategories AS osc  where osc.UserId = @userId and osc.SubCategoryId = sc.SubCategoryId)
-                                      THEN 1
-                                      ELSE 0
-                                      END) )
-                                      when 1 then 0
-                                      else sc.Price
-                                      end) as Price,
-                                      (case
-                                      when sc.Price = 0 then sc.PicturePath
-                                      when (SELECT
-                                      (CASE
-                                      WHEN EXISTS(
-                                      SELECT NULL AS emp
-                                      FROM OpenSubCategories AS osc  where osc.UserId = @userId and osc.SubCategoryId = sc.SubCategoryId
-                                      ) THEN 1
-                                      ELSE 0
-                                      END) ) = 1 then sc.PicturePath
-                                      ELSE @picturePath
-                                      end) as PicturePath
-                                      FROM FollowSubCategories fc
-                                      INNER JOIN SubCategoryLangs scl ON sc.SubCategoryId = fc.SubCategoryId
-                                      INNER JOIN SubCategories sc ON sc.SubCategoryId = scl.SubCategoryId
-                                      INNER JOIN SubCategoryRls scr ON scr.SubCategoryId = sc.SubCategoryId
-                                      INNER JOIN CategoryLocalizeds cl ON cl.CategoryId = scr.CategoryId AND cl.`Language` = @language
-                                      WHERE fsc.UserId = @userId AND scl.SubCategoryName LIKE '%{searchText}%' AND scl.`Language` = @language AND sc.Visibility = 1";
+            string sql = $@"SELECT 
+                            scl.SubCategoryName,
+                            sc.SubCategoryId,
+                            2 AS SearchType,
+                            cl.Text AS CategoryName,
+                            '0' AS DisplayPrice,
+                            sc.PicturePath
+                            FROM FollowSubCategories fsc
+                            INNER JOIN SubCategories sc ON sc.SubCategoryId = fsc.SubCategoryId AND sc.Visibility = 1
+                            INNER JOIN SubCategoryLangs scl ON sc.SubCategoryId = scl.SubCategoryId AND scl.`Language` = @language
+                            INNER JOIN SubCategoryRls scr ON scr.SubCategoryId = sc.SubCategoryId
+                            INNER JOIN CategoryLocalizeds cl ON cl.CategoryId = scr.CategoryId AND cl.`Language` = @language
+                            WHERE fsc.UserId = @userId AND scl.SubCategoryName LIKE '%{searchText}%'";
 
             return _followSubCategoryRepository.ToServiceModel<SearchModel>(sql, new
             {
                 userId,
                 language,
-                picturePath = DefaultImages.DefaultLock,
             }, pagingModel: pagingModel);
         }
 
