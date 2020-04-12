7fe6feb1fa5ca876c6bc1755472c00b15275a3ea
Ali Aldemir <ali.aldemir@biletkolik.com>
2020-04-13 02:51:51 +0300

    Update Startup.cs

--- src/Services/Category/ContestPark.Category.API/Startup.cs
+++ src/Services/Category/ContestPark.Category.API/Startup.cs
@@ -29,6 +29,7 @@ namespace ContestPark.Category.API
         {
             Configuration = configuration;
         }
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
