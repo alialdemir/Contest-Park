0fea731dd8604038d8b98b5f58d1fcf3120d4195
Ali Aldemir <ali.aldemir@biletkolik.com>
2020-04-29 13:45:12 +0300

    chat service migration 3.1

--- src/Services/Chat/ContestPark.Chat.API/Startup.cs
+++ src/Services/Chat/ContestPark.Chat.API/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using ServiceStack.Redis;
@@ -37,10 +38,10 @@ namespace ContestPark.Chat.API
 
             services.AddAuth(Configuration)
                     .AddMySql()
-                    .AddMvc()
+                    .AddMvc(options=> options.EnableEndpointRouting=false)
                     .AddJsonOptions()
                     .AddDataAnnotationsLocalization(typeof(ChatResource).Name)
-                    .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+                    .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
 
             services.AddSingleton<IRedisClient>(sp =>
             {
@@ -70,7 +71,7 @@ namespace ContestPark.Chat.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
