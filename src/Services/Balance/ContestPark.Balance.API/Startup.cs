fd1eac08fe9ade2c5756a6fc1755ddd7595ede97
Ali Aldemir <ali.aldemir@biletkolik.com>
2020-04-29 14:06:47 +0300

    background tasks migration 3.1

--- src/Services/Balance/ContestPark.Balance.API/Startup.cs
+++ src/Services/Balance/ContestPark.Balance.API/Startup.cs
@@ -18,6 +18,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 
@@ -41,10 +42,10 @@ namespace ContestPark.Balance.API
 
             services.AddAuth(Configuration)
                     .AddMySql()
-                    .AddMvc()
+                    .AddMvc(options=> options.EnableEndpointRouting=false)
                     .AddJsonOptions()
                     .AddDataAnnotationsLocalization(typeof(BalanceResource).Name)
-                    .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+                    .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
 
             services.AddLocalizationCustom();
 
@@ -71,7 +72,7 @@ namespace ContestPark.Balance.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
